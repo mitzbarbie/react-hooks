bee9c216dd8c739d64a0a21f7ed1b5fa71a2e0dd
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-23 17:13:04 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function App() {
-  return <div className="App"></div>;
+  const [value, setValue] = useState(0);
+  const plusItem = () => setValue(value + 1);
+  const minusItem = () => setValue(value - 1);
+
+  return (
+    <div className="App">
+      <h1>{value}</h1>
+      <button onClick={plusItem}>plus</button>
+      <button onClick={minusItem}>minus</button>
+    </div>
+  );
 }
 
 export default App;
