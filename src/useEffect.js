e86af6db46b034d9ccfe45e69b88f989e10f122d
SeungHwan Hong <mitzbarbie@naver.com>
2022-05-24 13:48:59 +0900

    useEffect-1

--- src/useEffect.js
+++ src/useEffect.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 function App() {
   const sayHello = () => console.log("hallo");
-  useEffect(() => {
-    sayHello();
-  });
+  //   useEffect(() => {
+  //     sayHello();
+  //   });
 
   const [number, setNumber] = useState(0);
   const [aNumber, setAnumber] = useState(0);
+  useEffect(sayHello, [number]);
 
   return (
     <div className="App">
